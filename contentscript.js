8a6086ffd4cef265750c13e29a62b1037b2928fd
Kerry <kerry.kim.russo@gmail.com>
2017-08-08 11:35:15 -0400

    fix for page anchor issue when it is used as placeholder

--- contentscript.js
+++ contentscript.js
@@ -3,17 +3,13 @@
         let node = e.target;
         let href = node.getAttribute('href');
         
-        if(!href) return;
+        // check for null href or page anchors (since a # could just be a placeholder)
+        if(!href || href.startsWith('#')) return;
         
         // if the user is holding the cmd key or the href is a full path, load in the same window
         if(e.metaKey || !/(\w+:\/\/)|(\W\w+%3A%2F%2F)/.test(href)) {
             if( true /*check user setting for cmd + clicks*/) window.location = href;
         } else {
-            // try {
-            // console.log(chrome.tabs.create({ active: false }))
-            // } catch(err) { console.log(err) }
-            // debugger;
-            // chrome.tabs.create({ active: false });
             chrome.runtime.connect().postMessage({ "url" : href });
         }
         e.preventDefault();    
