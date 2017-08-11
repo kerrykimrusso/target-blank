a6d17e0792c8c547a0cdc811d5a466b8c82e4e36
BJ Cantlupe <bjcantlupe@gmail.com>
2017-08-10 23:10:59 -0400

    add starter code for key capture input

--- contentscript.js
+++ contentscript.js
@@ -7,13 +7,13 @@
         // check for null href or page anchors (since a # could just be a placeholder)
         if(!href || href.startsWith('#')) return;
         
-        a.addEventListener('click', e => {   
-            e.preventDefault();    
+        a.addEventListener('click', e => {       
             
             // if the user is holding the cmd key or the href is a full path, load in the same window
             if(e.metaKey || !/(\w+:\/\/)|(\W\w+%3A%2F%2F)/.test(href)) {
                // if( true /*check user setting for cmd + clicks*/) window.location = href;
             } else {
+                e.preventDefault();
                 chrome.runtime.connect().postMessage({ "url" : href });
             }
         });
