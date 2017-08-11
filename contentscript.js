5cf38e1d89ce238e59aa86f209c5531a0bf89cc9
Kerry <kerry.kim.russo@gmail.com>
2017-08-10 21:42:49 -0400

    check anchor tag before adding listener

--- contentscript.js
+++ contentscript.js
@@ -1,19 +1,19 @@
 (function() {
-    const aTags = document.querySelectorAll("a");
-    aTags.forEach(a => a.addEventListener('click', e => {   
-        e.preventDefault();    
-        let node = e.target;
-        let href = node.getAttribute('href');
-        
+    const anchors = document.querySelectorAll("a");
+    anchors.forEach(a => {
+        let href = a.getAttribute('href');
         // check for null href or page anchors (since a # could just be a placeholder)
         if(!href || href.startsWith('#')) return;
         
-        // if the user is holding the cmd key or the href is a full path, load in the same window
-        if(e.metaKey || !/(\w+:\/\/)|(\W\w+%3A%2F%2F)/.test(href)) {
-            if( true /*check user setting for cmd + clicks*/) window.location = href;
-        } else {
-            chrome.runtime.connect().postMessage({ "url" : href });
-        }
-
-    }));
+        a.addEventListener('click', e => {   
+            e.preventDefault();    
+            
+            // if the user is holding the cmd key or the href is a full path, load in the same window
+            if(e.metaKey || !/(\w+:\/\/)|(\W\w+%3A%2F%2F)/.test(href)) {
+                if( true /*check user setting for cmd + clicks*/) window.location = href;
+            } else {
+                chrome.runtime.connect().postMessage({ "url" : href });
+            }
+        });
+    });
 })();
