80491e923adb0d6eb1f3231e72fd98e93d7f8f28
Kerry <kerry.kim.russo@gmail.com>
2017-09-14 00:27:26 -0400

    comply with eslint settings

--- contentscript.js
+++ contentscript.js
@@ -1,83 +1,9 @@
 (function init() {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     chrome.storage.sync.get(null, resolve);
   });
-})()
-.then(options => {
-
-  const anchors = document.querySelectorAll('a');
-
-  chrome.storage.onChanged.addListener(onOptionsChanged);
-
-  anchors.forEach((anchor) => {
-    const href = anchor.href;
-
-    if (window.strategy && window.strategy.matchesDomain(window.location.origin)) {
-      if (window.strategy.shouldIgnore(anchor)) return;
-    }
-
-    anchor.addEventListener('mousedown', tabOption(anchor));
-  });
-
-  function onOptionsChanged(changes) {
-    Object.keys(changes).forEach((key) => {
-      options[key] = changes[key].newValue;
-    });
-  }
-
-  function anchorType(anchor) {
-    const href = anchor.getAttribute('href');
-    const fullPath = anchor.href;
-
-    if (!href || href.startsWith('#') || !!anchor.onclick) return 'button';
-    if (fullPath.startsWith(`http://${window.location.host}`) || fullPath.startsWith(`https://${window.location.host}`)) {
-      return 'relative';
-    } else {
-      return 'absolute';
-    }
-  }
-
-  function tabOption(anchor) {
-
-    const type = anchorType(anchor);
-
-    switch (options[type]) {
-
-      case 'new-tab':
-        return (e) => {
-          // if the user is holding the cmd key or the href is a full path, load in the same window
-
-          // ignore if middle or right click
-          if (e.which > 1 && e.which < 4) return;
-
-          if (shouldDoOpposite(e)) {
-            openInSameTab();
-          } else {
-            e.preventDefault();
-            openInNewTab(anchor.href);
-          }
-        }
-
-      case 'same-tab':
-        anchor.target = '';
-        return (e) => {
-          // if the user is holding the cmd key or the href is a full path, load in the same window
-
-          // ignore if middle or right click
-          if (e.which > 1 && e.which < 4) return;
-
-          if (shouldDoOpposite(e)) {
-            e.preventDefault();
-            openInNewTab(anchor.href);
-          } else {
-            openInSameTab();
-          }
-        }
-
-      default:
-        return () => true;
-    }
-
+}())
+  .then((options) => {
     function openInSameTab() {
       return true;
     }
@@ -95,10 +21,73 @@
     function shouldDoOpposite(e) {
       return options.key === 'command' ? e.metaKey : e.altKey;
     }
-  }
 
-});
+    function onOptionsChanged(changes) {
+      Object.keys(changes).forEach((key) => {
+        options[key] = changes[key].newValue;
+      });
+    }
 
+    function anchorType(anchor) {
+      const href = anchor.getAttribute('href');
+      const fullPath = anchor.href;
 
-// Proxy link check???
-// !/(\w+:\/\/)|(\W\w+%3A%2F%2F)/.test(href)
+      if (!href || href.startsWith('#') || !!anchor.onclick) return 'button';
+      if (fullPath.startsWith(`http://${window.location.host}`) || fullPath.startsWith(`https://${window.location.host}`)) {
+        return 'relative';
+      }
+      return 'absolute';
+    }
+
+    function tabOption(anchor) {
+      const type = anchorType(anchor);
+
+      switch (options[type]) {
+        case 'new-tab':
+          return (e) => {
+          // if the user is holding the cmd key or the href is a full path, load in the same window
+
+          // ignore if middle or right click
+            if (e.which > 1 && e.which < 4) return;
+
+            if (shouldDoOpposite(e)) {
+              openInSameTab();
+            } else {
+              e.preventDefault();
+              openInNewTab(anchor.href);
+            }
+          };
+
+        case 'same-tab':
+          anchor.target = '';
+          return (e) => {
+          // if the user is holding the cmd key or the href is a full path, load in the same window
+
+          // ignore if middle or right click
+            if (e.which > 1 && e.which < 4) return;
+
+            if (shouldDoOpposite(e)) {
+              e.preventDefault();
+              openInNewTab(anchor.href);
+            } else {
+              openInSameTab();
+            }
+          };
+
+        default:
+          return () => true;
+      }
+    }
+
+    const anchors = document.querySelectorAll('a');
+
+    chrome.storage.onChanged.addListener(onOptionsChanged);
+
+    anchors.forEach((anchor) => {
+      if (window.strategy && window.strategy.matchesDomain(window.location.origin)) {
+        if (window.strategy.shouldIgnore(anchor)) return;
+      }
+
+      anchor.addEventListener('mousedown', tabOption(anchor));
+    });
+  });
