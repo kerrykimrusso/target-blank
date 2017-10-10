56c3a7ebedce5ffb8bb4af39d13f44af410e2d1d
Kerry <kerry.kim.russo@gmail.com>
2017-10-10 19:15:12 -0400

    fix eslint errors

--- utils.js
+++ utils.js
@@ -1,13 +1,12 @@
-const utils = (function() {
-  
+const utils = (function initUtils() {
   const isSubdomain = function isSubdomain(a, b) {
-    const regex = /(?:\w+\.)*(\w+\.\w+)/,
-    matchA = a.match(regex),
-    matchB = b.match(regex);
+    const regex = /(?:\w+\.)*(\w+\.\w+)/;
+    const matchA = a.match(regex);
+    const matchB = b.match(regex);
     return matchA && matchB && matchA[1] === matchB[1];
-  }
+  };
 
   return {
     isSubdomain,
-  }
-})()
+  };
+}());
