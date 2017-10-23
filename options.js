53e69396a21b48aa7980af1c92315dd96d43fef1
Kerry <kerry.kim.russo@gmail.com>
2017-10-23 16:44:12 -0400

    restore sleep toggle state from storage; clear status text if state is off

--- options.js
+++ options.js
@@ -1,79 +1,109 @@
-document.addEventListener('DOMContentLoaded', () => {
-  function saveOptions(e) {
-    e.preventDefault();
+const init = (function init(utils) {
+  return () => {
+    function saveOptions(e) {
+      e.preventDefault();
 
-    const options = {};
-    const form = e.currentTarget;
-    // subtract 1 from form length so we do not iterate over save button
-    for (let i = 0, el; i < form.length; i += 1) {
-      el = form[i];
-      options[el.name] = el.value;
+      const options = {};
+      const form = e.currentTarget;
+      // subtract 1 from form length so we do not iterate over save button
+      for (let i = 0, el; i < form.length; i += 1) {
+        el = form[i];
+        options[el.name] = el.value;
+      }
+
+      chrome.runtime.sendMessage({
+        type: 'SAVE_OPTIONS_BTN_CLICKED',
+        payload: options,
+      });
     }
 
-    chrome.runtime.sendMessage({
-      type: 'SAVE_OPTIONS_BTN_CLICKED',
-      payload: options,
-    });
-  }
+    function restoreOptionsForm(options) {
+      // looping over keys works for now if we're sticking to all dropdown lists
+      Object.keys(options).forEach((key) => {
+        const el = document.querySelector(`select[name=${key}]`);
+        if (el) el.namedItem(options[key]).setAttribute('selected', true);
+      });
+    }
 
-  function restoreOptionsForm(options) {
-    // looping over keys works for now if we're sticking to all dropdown lists
-    Object.keys(options).forEach((key) => {
-      const el = document.querySelector(`select[name=${key}]`);
-      if (el) el.namedItem(options[key]).setAttribute('selected', true);
-    });
-  }
+    function onOptionsSaved() {
+      // const status = document.getElementById('status');
+      // status.textContent = 'Options saved.';
+      // setTimeout(() => {
+      //   status.textContent = '';
+      // }, 750);
+    }
 
-  function onOptionsSaved() {
-    // const status = document.getElementById('status');
-    // status.textContent = 'Options saved.';
-    // setTimeout(() => {
-    //   status.textContent = '';
-    // }, 750);
-  }
+    function displaySleepTimerStatus(expirationTimeFormatted) {
+      const status = document.querySelector('#sleepToggleForm .status');
+      status.textContent = `Will wake up at ${expirationTimeFormatted} tomorrow`;
+    }
 
-  function onSleepTimerSet(expirationTimeInMs) {
-    const status = document.querySelector('#sleepToggleForm .status');
-    const expirationDate = new Date(expirationTimeInMs);
-    const timeMatches = expirationDate.toLocaleTimeString().match(/(\d+.\d+).\d+(\s+\w+)*/i);
-    const [, time, meridiem] = timeMatches;
-    status.textContent = `Will wake up at ${time}${meridiem || ''} tomorrow`;
-  }
+    function clearSleepTimerStatus() {
+      const status = document.querySelector('#sleepToggleForm .status');
+      status.textContent = '';
+    }
 
-  function setSleepTimer(duration = 0) {
-    chrome.runtime.sendMessage({
-      type: 'SET_SLEEP_TIMER',
-      payload: duration,
-    });
-  }
+    function formatSleepTimer(expirationTimeInMs) {
+      const expirationDate = new Date(expirationTimeInMs);
+      const timeMatches = expirationDate.toLocaleTimeString().match(/(\d+.\d+).\d+(\s+\w+)*/i);
+      const [, time, meridiem] = timeMatches;
+      return `${time}${meridiem || ''}`;
+    }
 
-  chrome.storage.sync.get(null, (options) => {
-    restoreOptionsForm(options);
-
-    chrome.runtime.onMessage.addListener((msg) => {
-      const messageHandlers = {
-        OPTIONS_SAVED: onOptionsSaved,
-        SLEEP_TIMER_SET: onSleepTimerSet,
-      };
-
-      messageHandlers[msg.type](msg.payload);
-    });
-
-    document.getElementById('optionsForm').addEventListener('change', saveOptions);
-    document.getElementById('sleepTimerForm').addEventListener('submit', (e) => {
-      e.preventDefault();
-      setSleepTimer(e.target.duration.value);
-    });
-    document.querySelector('#sleepToggleForm').toggle.addEventListener('change', (e) => {
-      e.preventDefault();
-
-      if (e.target.checked) {
-        const today = new Date();
-        const expirationDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1, 2, 0, 0);
-        setSleepTimer(expirationDate - today);
+    function onSleepTimerSet(expirationTimeInMs) {
+      if (utils.isSleepTimerEnabled(expirationTimeInMs, Date.now())) {
+        displaySleepTimerStatus(formatSleepTimer(expirationTimeInMs));
       } else {
-        setSleepTimer();
+        clearSleepTimerStatus();
       }
+    }
+
+    function setSleepTimer(duration = 0) {
+      chrome.runtime.sendMessage({
+        type: 'SET_SLEEP_TIMER',
+        payload: duration,
+      });
+    }
+
+    function restoreSleepTimerForm(options) {
+      const el = document.querySelector('#sleepToggleForm').toggle;
+      if (utils.isSleepTimerEnabled(options.expiration, Date.now())) {
+        el.setAttribute('checked', 1);
+        onSleepTimerSet(options.expiration);
+      }
+    }
+
+    chrome.storage.sync.get(null, (options) => {
+      restoreOptionsForm(options);
+      restoreSleepTimerForm(options);
+
+      chrome.runtime.onMessage.addListener((msg) => {
+        const messageHandlers = {
+          OPTIONS_SAVED: onOptionsSaved,
+          SLEEP_TIMER_SET: onSleepTimerSet,
+        };
+
+        messageHandlers[msg.type](msg.payload);
+      });
+
+      document.getElementById('optionsForm').addEventListener('change', saveOptions);
+      document.getElementById('sleepTimerForm').addEventListener('submit', (e) => {
+        e.preventDefault();
+        setSleepTimer(e.target.duration.value);
+      });
+      document.querySelector('#sleepToggleForm').toggle.addEventListener('change', (e) => {
+        e.preventDefault();
+
+        if (e.target.checked) {
+          const today = new Date();
+          const expirationDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1, 2, 0, 0);
+          setSleepTimer(expirationDate - today);
+        } else {
+          setSleepTimer();
+        }
+      });
     });
-  });
-});
+  };
+}(window.utils));
+
+document.addEventListener('DOMContentLoaded', init);
