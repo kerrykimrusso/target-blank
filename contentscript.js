fc8f215f62ff43a85d9228186040119ccb64ff75
Kerry Russo <kerry.kim.russo@gmail.com>
2017-12-10 19:09:57 -0500

    update add/removeClickHandlers to accept list of anchors

--- contentscript.js
+++ contentscript.js
@@ -1,54 +1,5 @@
-const init = (function init({ utils, strategy, location, enums, MutationSummary }) {
+const init = (function init({ utils, strategy, location, enums }, MutationSummary) {
   let mutationObserver;
-  
-  const addClickHandlers = () => {
-    const anchors = document.querySelectorAll('a');
-    anchors.forEach((a) => {
-      a.addEventListener('click', onAnchorClicked);
-    });
-  };
-
-  const removeClickHandlers = () => {
-    const anchors = document.querySelectorAll('a');
-    anchors.forEach((a) => {
-      a.removeEventListener('click', onAnchorClicked);
-    });
-  }
-
-  utils.getOptions()
-    .then((options) => {
-      chrome.runtime.onMessage.addListener((msg) => {
-        const messageHandlers = {
-          [enums.SAVE_OPTIONS_SUCCEEDED]: (newOptions) => {
-            const newPrefs = newOptions[location.hostname];
-            if (newPrefs && !utils.isSleepTimerEnabled(newPrefs.expiration, Date.now())) {
-              console.log('adding click handler...')
-              addClickHandlers();
-            } else {
-              removeClickHandlers();
-            }
-          },
-        };
-        messageHandlers[msg.type](msg.payload);
-      });
-      const prefs = options[location.hostname];
-      if (prefs && !utils.isSleepTimerEnabled(prefs.expiration, Date.now())) {
-        addClickHandlers();
-      }
-      mutationObserver = new MutationSummary({
-        callback: (summaryObjects) => {
-          if (prefs && !utils.isSleepTimerEnabled(prefs.expiration, Date.now())) {
-            summaryObjects[0].added.forEach(
-              a => a.addEventListener('click', onAnchorClicked),
-            );
-          }
-        },
-        queries: [
-          { element: 'a' },
-        ],
-      });
-    })
-    .catch(console.log);
 
   const onAnchorClicked = (e) => {
     if (e.which > 1 && e.which < 4) return;
@@ -65,4 +16,51 @@ const init = (function init({ utils, strategy, location, enums, MutationSummary 
       },
     );
   };
-}({ utils: window.utils, strategy: window.strategy, location: window.location, enums: window.enums, MutationSummary }));
+
+  const addClickHandlers = (anchors) => {
+    anchors.forEach((a) => {
+      a.addEventListener('click', onAnchorClicked);
+    });
+  };
+
+  const removeClickHandlers = (anchors) => {
+    anchors.forEach((a) => {
+      a.removeEventListener('click', onAnchorClicked);
+    });
+  };
+
+  utils.getOptions()
+    .then((options) => {
+      chrome.runtime.onMessage.addListener((msg) => {
+        const messageHandlers = {
+          [enums.SAVE_OPTIONS_SUCCEEDED]: (newOptions) => {
+            const newPrefs = newOptions[location.hostname];
+            const allAnchors = document.querySelectorAll('a');
+            if (newPrefs && !utils.isSleepTimerEnabled(newPrefs.expiration, Date.now())) {
+              addClickHandlers(allAnchors);
+            } else {
+              removeClickHandlers(allAnchors);
+            }
+          },
+        };
+        messageHandlers[msg.type](msg.payload);
+      });
+      const prefs = options[location.hostname];
+      if (prefs && !utils.isSleepTimerEnabled(prefs.expiration, Date.now())) {
+        addClickHandlers(document.querySelectorAll('a'));
+      }
+
+      mutationObserver = new MutationSummary({
+        callback: (summaryObjects) => {
+          if (prefs && !utils.isSleepTimerEnabled(prefs.expiration, Date.now())) {
+            addClickHandlers(summaryObjects[0].added);
+          }
+        },
+        queries: [
+          { element: 'a' },
+        ],
+      });
+    })
+    .catch(console.log);
+/* eslint-disable no-undef */
+}(window, MutationSummary));
